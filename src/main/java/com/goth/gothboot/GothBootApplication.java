aeb035985df6bf80ce15618f05fe7c0aaca7dff3
yuan,jianfeng <tashiboy@126.com>
2020-11-25 21:54:10 +0800

    Initial commit

---
+++ src/main/java/com/goth/gothboot/GothBootApplication.java
@@ -0,0 +1,13 @@
+package com.goth.gothboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GothBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GothBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goth/gothboot/GothBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goth.gothboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GothBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
